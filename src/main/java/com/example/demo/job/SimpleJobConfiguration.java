db4abd5f3d5832b27de7cfd365d6e5ec715627ec
sw.cho <progress0407@naver.com>
2021-07-17 14:41:34 +0900

    rename class name

--- src/main/java/com/example/demo/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/demo/job/SimpleJobConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
-public class SImpleJobConfiguration {
+public class SimpleJobConfiguration {
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
