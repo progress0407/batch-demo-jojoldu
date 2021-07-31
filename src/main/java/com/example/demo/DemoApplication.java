07054cfdab1440090a9f05af6ffe17a657174e0a
swcho <progress0407@naver.com>
2021-07-31 21:18:25 +0900

    write : itemReader

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableBatchProcessing // 배치기능 활성화
 @SpringBootApplication // (exclude = {DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) // jar 생성시 문제 DB설정 자동화..
 public class DemoApplication {
 
 	public static void main(String[] args) {
