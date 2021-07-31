3156d2240c1d9492a84955b52ea392a0eb36c4af
swcho <progress0407@naver.com>
2021-07-31 22:29:36 +0900

    8장 writer까지 ... boot를 공부후 9장부터 하자

---
+++ src/main/java/com/example/demo/job/Pay2.java
@@ -0,0 +1,41 @@
+package com.example.demo.job;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@ToString
+@Getter @Setter
+@NoArgsConstructor
+@Entity
+public class Pay2 {
+
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long amount;
+    private String txName;
+    private LocalDateTime txDateTime;
+
+    public Pay2(Long amount, String txName, LocalDateTime txDateTime) {
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+
+    public Pay2(Long id, Long amount, String txName, LocalDateTime txDateTime) {
+        this.id = id;
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+}
